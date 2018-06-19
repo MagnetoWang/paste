42883f4f467dcc45336096b5bf4528388daf7855
MagnetoWang <1527672216@qq.com>
2018-06-19 12:08:30 +0800

    完成读写excel模块

---
+++ README.md
@@ -0,0 +1,16 @@
+# paste
+
+### 专注于可复制可粘帖的代码项目
+
+
+
+## 文件说明-src/main/java下的目录
+
+#### fileUtils   -------------  文件的相关操作
+
+- readFile:读文件的基本模板
+- RWExcel:读写excel文件
+
+
+
+
---
+++ paste/src/main/java/fileUtils/RWExcel.java
@@ -0,0 +1,190 @@
+package fileUtils;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+
+public class RWExcel {
+
+    public static void main(String[] args) {
+//        writeExcel();
+        readExcel();
+
+    }
+
+    /**
+     * @see. 写入excel文件 。后缀名 xls
+     */
+    public static void writeExcel(){
+        // 创建一个Excel文件
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        // 创建一个工作表
+        HSSFSheet sheet = workbook.createSheet("学生表一");
+        // 添加表头行
+        HSSFRow hssfRow = sheet.createRow(0);
+        // 设置单元格格式居中
+        HSSFCellStyle cellStyle = workbook.createCellStyle();
+//        cellStyle.setAlignment(HSSFCellStyle.ALIGN_LEFT);
+
+        // 添加表头内容
+        HSSFCell headCell = hssfRow.createCell(0);
+        headCell.setCellValue("query");
+        headCell.setCellStyle(cellStyle);
+
+        headCell = hssfRow.createCell(1);
+        headCell.setCellValue("type");
+        headCell.setCellStyle(cellStyle);
+
+        headCell = hssfRow.createCell(2);
+        headCell.setCellValue("name");
+        headCell.setCellStyle(cellStyle);
+        //插入数据模块
+        for (int i = 0; i < 10; i++) {
+            hssfRow = sheet.createRow((int) i + 1);
+
+
+            // 创建单元格，并设置值
+            HSSFCell cell = hssfRow.createCell(0);
+            cell.setCellValue("queryRow");
+            cell.setCellStyle(cellStyle);
+
+            cell = hssfRow.createCell(1);
+            cell.setCellValue("typeRow");
+            cell.setCellStyle(cellStyle);
+
+            cell = hssfRow.createCell(2);
+            cell.setCellValue("汉字");
+            cell.setCellStyle(cellStyle);
+        }
+        //保存文件代码
+        try {
+            OutputStream outputStream = new FileOutputStream("src/resourse/test.xls");
+            workbook.write(outputStream);
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("write end");
+    }
+
+    /**
+     * 注意文件地址位置
+     * 读excel 文件
+     */
+    public static void readExcel(){
+        List<Student> list = new ArrayList<Student>();
+        HSSFWorkbook workbook = null;
+
+        try {
+            // 读取Excel文件
+            InputStream inputStream = new FileInputStream("src/resourse/test.xls");
+            workbook = new HSSFWorkbook(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 循环工作表
+        for (int numSheet = 0; numSheet < workbook.getNumberOfSheets(); numSheet++) {
+            HSSFSheet hssfSheet = workbook.getSheetAt(numSheet);
+            if (hssfSheet == null) {
+                continue;
+            }
+            // 循环行
+            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+                HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+                if (hssfRow == null) {
+                    continue;
+                }
+
+                // 将单元格中的内容存入集合
+                Student student = new Student();
+
+                HSSFCell cell = hssfRow.getCell(0);
+                if (cell == null) {
+                    continue;
+                }
+                student.setName(cell.getStringCellValue());
+
+                cell = hssfRow.getCell(1);
+                if (cell == null) {
+                    continue;
+                }
+                student.setAge((int) cell.getNumericCellValue());
+
+                cell = hssfRow.getCell(2);
+                if (cell == null) {
+                    continue;
+                }
+                student.setGrade(cell.getStringCellValue());
+
+                list.add(student);
+            }
+        }
+        //打印读取的数据
+        for(Student e : list){
+            System.out.println(e.Age+" "+ e.Grade);
+        }
+//        return list;
+
+    }
+    static class Student{
+        public String getQuery() {
+            return query;
+        }
+
+        public void setQuery(String query) {
+            this.query = query;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        private String query;
+        private String type;
+        private String name;
+        private int Age;
+
+        public int getAge() {
+            return Age;
+        }
+
+        public void setAge(int age) {
+            Age = age;
+        }
+
+        public String getGrade() {
+            return Grade;
+        }
+
+        public void setGrade(String grade) {
+            Grade = grade;
+        }
+
+        private String Grade;
+    }
+}
