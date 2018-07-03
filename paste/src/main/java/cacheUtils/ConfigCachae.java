4912d2a7aa0be8ceab29c30e6ef9b6acd041c695
MagnetoWang <1527672216@qq.com>
2018-07-03 10:49:45 +0800

    新增ehcache3版的自定义存活模式。需要覆盖接口重新实现。
    注意一点，生成一个实例以后，存活时间是定下来的。无法更改。要想主观上更改存活时间，必须先删除实例，再新建一个实例才行。新建的实例就是新的存活时间

--- paste/src/main/java/cacheUtils/ConfigCachae.java
+++ paste/src/main/java/cacheUtils/ConfigCachae.java
@@ -3,6 +3,7 @@ package cacheUtils;
 import org.ehcache.*;
 import org.ehcache.config.Builder;
 import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.CacheRuntimeConfiguration;
 import org.ehcache.config.Configuration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
 import org.ehcache.config.builders.CacheManagerBuilder;
@@ -10,10 +11,12 @@ import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.builders.UserManagedCacheBuilder;
 import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
+import org.ehcache.core.config.ExpiryUtils;
 import org.ehcache.core.spi.service.LocalPersistenceService;
 import org.ehcache.impl.config.persistence.DefaultPersistenceConfiguration;
 import org.ehcache.impl.config.persistence.UserManagedPersistenceContext;
 import org.ehcache.impl.persistence.DefaultLocalPersistenceService;
+import org.ehcache.spi.service.ServiceConfiguration;
 import org.ehcache.xml.XmlConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,7 +52,7 @@ public class ConfigCachae {
      * http://www.ehcache.org/documentation/3.5/
      */
     public static void config(){
-        final URL myUrl = ConfigCachae.class.getResource("ehcache.xml");
+        final URL myUrl = ConfigCachae.class.getResource("/cache/ehcache.xml");
         System.out.println(myUrl);
         Configuration xmlConfig = new XmlConfiguration(myUrl);
         CacheManager myCacheManager = CacheManagerBuilder.newCacheManager(xmlConfig);
@@ -59,9 +62,20 @@ public class ConfigCachae {
 
         Cache<String ,String> mycache=myCacheManager.getCache("mycache",String.class, String.class);
         mycache.put("test","哈哈");
+        CacheRuntimeConfiguration<String ,String> c= mycache.getRuntimeConfiguration();
+        for(ServiceConfiguration e: c.getServiceConfigurations()){
+            e.getServiceType();
+
+        }
+        System.out.println(myCacheManager.getRuntimeConfiguration());
+
+
         System.out.println(new Date());
+        //测试存活时间，是否和规定的一样
         while (true){
+
             if(mycache.containsKey("test")==false){
+
                 System.out.println(new Date());
                 break;
             }
---
+++ paste/src/main/java/cacheUtils/UpdateExpire.java
@@ -0,0 +1,97 @@
+package cacheUtils;
+
+import org.apache.commons.collections.map.LinkedMap;
+import org.ehcache.Cache;
+import org.ehcache.CacheManager;
+import org.ehcache.config.CacheConfiguration;
+import org.ehcache.config.Configuration;
+import org.ehcache.config.builders.CacheConfigurationBuilder;
+import org.ehcache.config.builders.CacheManagerBuilder;
+import org.ehcache.config.builders.ResourcePoolsBuilder;
+import org.ehcache.core.config.ExpiryUtils;
+import java.time.Duration;
+
+import org.ehcache.expiry.Expirations;
+import org.ehcache.expiry.ExpiryPolicy;
+import org.ehcache.xml.XmlConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
+import java.util.Date;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static java.lang.Thread.sleep;
+
+/***
+ * ehcache3 采取一个生命周期的概念来操作动态运行程序的缓存时间
+ */
+public class UpdateExpire {
+    private  static Logger logger= LoggerFactory.getLogger(ConfigCachae.class);
+
+    public static void main(String[] args) throws InterruptedException {
+        fragment1();
+    }
+
+    public static void fragment1() throws InterruptedException {
+        final URL myUrl = ConfigCachae.class.getResource("/cache/ehcache.xml");
+        System.out.println(myUrl);
+        Configuration xmlConfig = new XmlConfiguration(myUrl);
+        CacheManager myCacheManager = CacheManagerBuilder.newCacheManager(xmlConfig);
+        myCacheManager.init();
+        CacheManager cacheManager = myCacheManager;
+        CacheConfigurationBuilder<Long, String> configuration =
+                CacheConfigurationBuilder.newCacheConfigurationBuilder(Long.class, String.class, ResourcePoolsBuilder
+                        .heap(100))
+                        .withExpiry(new ExpiryPolicy<Long, String>() {
+                            @Override
+                            public Duration getExpiryForCreation(Long key, String value) {
+                                return getTimeToLiveDuration(key, value);
+                            }
+
+                            @Override
+                            public Duration getExpiryForAccess(Long key, Supplier<? extends String> value) {
+                                return null;  // Keeping the existing expiry
+                            }
+
+                            @Override
+                            public Duration getExpiryForUpdate(Long key, Supplier<? extends String> oldValue, String newValue) {
+                                return null;  // Keeping the existing expiry
+                            }
+                        });
+        cacheManager.createCache("cache", configuration);
+
+        Cache<Long, String> cache = cacheManager.getCache("cache", Long.class, String.class);
+        cache.put(10L, "Hello");
+//        Map<String,String> content=(LinkedMap)value;
+
+
+        Long expireTime =new Long(3L);
+        cache.put(3L,expireTime.toString());
+
+//        Duration.ofSeconds(Long.parseLong(content.get("timeToLive")));
+
+        System.out.println(cache.get(10L));
+
+//        sleep(2100);
+
+// Now the returned value should be null, as mapping is expired.
+        System.out.println(cache.get(10L));
+    }
+    /**
+     * Returns the expiry duration for the given key/value pair, based on some complex logic.
+     * @param key Cache Key
+     * @param value Cache Value
+     * @return return expiry duration
+     */
+    private static Duration getTimeToLiveDuration(Long key, String value) {
+        // Returns TTL of 10 seconds for keys less than 1000
+        if (key < 1000) {
+            return Duration.ofSeconds(2);
+        }
+
+        // Otherwise return 5 seconds TTL
+        return Duration.ofSeconds(5);
+    }
+}
