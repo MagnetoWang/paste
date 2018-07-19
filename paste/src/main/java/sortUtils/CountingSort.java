249d03a43085378ca075087b5900481b7e73ca29
MagnetoWang <1527672216@qq.com>
2018-07-19 13:40:01 +0800

    添加了最后两个不太常用的算法

--- paste/src/main/java/sortUtils/CountingSort.java
+++ paste/src/main/java/sortUtils/CountingSort.java
@@ -1,10 +1,49 @@
 package sortUtils;
 
+import java.util.Arrays;
+
 /**
  * @program: paste
  * @description: 计数排序算法
- * @author: MagnetoWang
  * @create: 2018-07-19 12:15
+ * 源码链接：https://github.com/hustcc/JS-Sorting-Algorithm/blob/master/src/java/main/CountingSort.java
  **/
-public class CountingSort {
+public class CountingSort implements SortTemplate{
+    @Override
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int maxValue = getMaxValue(arr);
+
+        return countingSort(arr, maxValue);
+    }
+
+    private int[] countingSort(int[] arr, int maxValue) {
+        int bucketLen = maxValue + 1;
+        int[] bucket = new int[bucketLen];
+
+        for (int value : arr) {
+            bucket[value]++;
+        }
+
+        int sortedIndex = 0;
+        for (int j = 0; j < bucketLen; j++) {
+            while (bucket[j] > 0) {
+                arr[sortedIndex++] = j;
+                bucket[j]--;
+            }
+        }
+        return arr;
+    }
+
+    private int getMaxValue(int[] arr) {
+        int maxValue = arr[0];
+        for (int value : arr) {
+            if (maxValue < value) {
+                maxValue = value;
+            }
+        }
+        return maxValue;
+    }
 }
--- paste/src/main/java/sortUtils/ShellSort.java
+++ paste/src/main/java/sortUtils/ShellSort.java
@@ -1,10 +1,37 @@
 package sortUtils;
 
+import java.util.Arrays;
+
 /**
  * @program: paste
  * @description: 希尔排序算法
- * @author: MagnetoWang
  * @create: 2018-07-19 12:14
+ * 源码链接：https://github.com/hustcc/JS-Sorting-Algorithm/blob/master/src/java/main/ShellSort.java
  **/
-public class ShellSort {
+public class ShellSort implements SortTemplate{
+    @Override
+    public int[] sort(int[] sourceArray) throws Exception {
+        // 对 arr 进行拷贝，不改变参数内容
+        int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
+
+        int gap = 1;
+        while (gap < arr.length) {
+            gap = gap * 3 + 1;
+        }
+
+        while (gap > 0) {
+            for (int i = gap; i < arr.length; i++) {
+                int tmp = arr[i];
+                int j = i - gap;
+                while (j >= 0 && arr[j] > tmp) {
+                    arr[j + gap] = arr[j];
+                    j -= gap;
+                }
+                arr[j + gap] = tmp;
+            }
+            gap = (int) Math.floor(gap / 3);
+        }
+
+        return arr;
+    }
 }
