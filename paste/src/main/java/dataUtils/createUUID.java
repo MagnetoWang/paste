7bab6ff106110e6700bc38df27d2c3b617312868
MagnetoWang <1527672216@qq.com>
2018-07-27 15:14:52 +0800

    新增stringbuilder清空数据的测试代码。delete方法效率高

--- paste/src/main/java/dataUtils/createUUID.java
+++ paste/src/main/java/dataUtils/createUUID.java
@@ -7,5 +7,7 @@ public class createUUID {
         String uuid = UUID.randomUUID().toString(); //获取UUID并转化为String对象
         uuid = uuid.replace("-", "");               //因为UUID本身为32位只是生成时多了“-”，所以将它们去点就可
         System.out.println(uuid);
+        Long number=new Long("123456789123456789");
+        System.out.println(number);
     }
 }
