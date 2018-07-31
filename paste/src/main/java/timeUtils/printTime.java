a3fc0e178fc5d5455b0f18ea724dcc7f48520717
MagnetoWang <1527672216@qq.com>
2018-07-31 16:33:28 +0800

    专门用来打印时间的类

---
+++ paste/src/main/java/timeUtils/printTime.java
@@ -0,0 +1,13 @@
+package timeUtils;
+
+/**
+ * @program: paste
+ * @description: 打印各种时间
+ * @author: MagnetoWang
+ * @create: 2018-07-31 14:15
+ **/
+public class printTime {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
