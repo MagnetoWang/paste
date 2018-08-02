3eb903ab7369042530ae523ac82ee05d30779f16
MagnetoWang <1527672216@qq.com>
2018-08-02 11:38:50 +0800

    定位json数组的位置
    很多代码都是因为数据的支撑才能进行下去。所以去大公司才能站在更高层次

--- paste/src/main/java/jsonUtils/LockLocation.java
+++ paste/src/main/java/jsonUtils/LockLocation.java
@@ -1,6 +1,8 @@
 package jsonUtils;
 
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 import java.util.Date;
@@ -12,6 +14,11 @@ import java.util.Date;
  * @create: 2018-07-26 09:41
  **/
 public class LockLocation {
+    public static void main(String[] args) {
+        String message="{\"docs\":[{\"strategy_info\":{\"fb_expo_id\":\"74891355140698112\",\"fb_item_location\":\"0\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698113\",\"fb_item_location\":\"1\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698114\",\"fb_item_location\":\"2\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698115\",\"fb_item_location\":\"3\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698116\",\"fb_item_location\":\"4\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698117\",\"fb_item_location\":\"5\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698118\",\"fb_item_location\":\"6\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698119\",\"fb_item_location\":\"7\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698120\",\"fb_item_location\":\"8\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698121\",\"fb_item_location\":\"9\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698122\",\"fb_item_location\":\"10\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698123\",\"fb_item_location\":\"11\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698124\",\"fb_item_location\":\"12\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698125\",\"fb_item_location\":\"13\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698126\",\"fb_item_location\":\"14\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698127\",\"fb_item_location\":\"15\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698128\",\"fb_item_location\":\"16\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698129\",\"fb_item_location\":\"17\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698130\",\"fb_item_location\":\"18\"}},{\"strategy_info\":{\"fb_expo_id\":\"74891355140698131\",\"fb_item_location\":\"19\"}}]}\n";
+        LockLocation lockLocation=new LockLocation();
+        lockLocation.getJsonArrayValue(message);
+    }
     /**
      *
      * @param message json格式消息包
@@ -64,5 +71,24 @@ public class LockLocation {
         }
         return object.getDate(lastWord[lastWord.length-1]);
     }
+
+    /**
+     * 定位json数组某个位置。感觉没有什么特别高效的方法啊
+     * @param message
+     * @return
+     */
+    public String getJsonArrayValue(String message){
+
+//        jsonObject.getJSONArray("strategy_info");
+        JSONArray jsonArray = JSONObject.parseObject(message).getJSONArray("docs");
+        for(Object e:jsonArray){
+            System.out.println(e.toString());
+            System.out.println(((JSONObject)e).getJSONObject("strategy_info").getString("fb_item_location"));
+        }
+
+        return "";
+    }
+    //{"docs":[{"strategy_info":{"fb_expo_id":"74891355140698112","fb_item_location":"0"}},{"strategy_info":{"fb_expo_id":"74891355140698113","fb_item_location":"1"}},{"strategy_info":{"fb_expo_id":"74891355140698114","fb_item_location":"2"}},{"strategy_info":{"fb_expo_id":"74891355140698115","fb_item_location":"3"}},{"strategy_info":{"fb_expo_id":"74891355140698116","fb_item_location":"4"}},{"strategy_info":{"fb_expo_id":"74891355140698117","fb_item_location":"5"}},{"strategy_info":{"fb_expo_id":"74891355140698118","fb_item_location":"6"}},{"strategy_info":{"fb_expo_id":"74891355140698119","fb_item_location":"7"}},{"strategy_info":{"fb_expo_id":"74891355140698120","fb_item_location":"8"}},{"strategy_info":{"fb_expo_id":"74891355140698121","fb_item_location":"9"}},{"strategy_info":{"fb_expo_id":"74891355140698122","fb_item_location":"10"}},{"strategy_info":{"fb_expo_id":"74891355140698123","fb_item_location":"11"}},{"strategy_info":{"fb_expo_id":"74891355140698124","fb_item_location":"12"}},{"strategy_info":{"fb_expo_id":"74891355140698125","fb_item_location":"13"}},{"strategy_info":{"fb_expo_id":"74891355140698126","fb_item_location":"14"}},{"strategy_info":{"fb_expo_id":"74891355140698127","fb_item_location":"15"}},{"strategy_info":{"fb_expo_id":"74891355140698128","fb_item_location":"16"}},{"strategy_info":{"fb_expo_id":"74891355140698129","fb_item_location":"17"}},{"strategy_info":{"fb_expo_id":"74891355140698130","fb_item_location":"18"}},{"strategy_info":{"fb_expo_id":"74891355140698131","fb_item_location":"19"}}]
+    //}},"timestamp":"1532591490478","fb_query_id":"74891355132325888","fb_service_id":"1011710017","ucid":"0","fb_ab_test_flag":"","request":{"index":"1011710017","qfs":["title","houseCode","resblockName"],"requestFunctionScores":[{"weight":1000.0,"filter":{"and":[{"or":[{"field":"tags","action":"match","value":"is_vr"}]}]}}],"queryOperator":"OR","requestId":"","query":"","strategyId":0,"fls":["houseCode"],"aggregations":[],"returnRelativeScore":false,"size":20,"cityIds":["440600"],"page":2,"queryMinimumShouldMatch":"-25%","queryFuzziness":-1,"sortedQueryMinimumShouldMatch":"1","sorts":[{"field":"priceTotal","order":"asc"}],"filters":{"and":[{"or":[{"field":"bizcircleId","action":"match","value":"1100000514"}]},{"or":[{"field":"appid","action":"match","value":"104"}]},{"or":[{"field":"cityId","action":"match","value":"440600"}]},{"or":[{"field":"houseType","action":"match","value":"107500000001"},{"field":"houseType","action":"match","value":"107500000002"},{"field":"houseType","action":"match","value":"107500000003"},{"field":"houseType","action":"match","value":"107500000004"},{"field":"houseType","action":"match","value":"107500000005"},{"field":"houseType","action":"match","value":"107500000009"},{"field":"houseType","action":"match","value":"107500000010"},{"field":"houseType","action":"match","value":"107500000012"},{"field":"houseType","action":"match","value":"107500000013"},{"field":"houseType","action":"match","value":"107500000014"},{"field":"houseType","action":"match","value":"107500000015"},{"field":"houseType","action":"match","value":"107500000017"},{"field":"houseType","action":"match","value":"107599999998"}]},{"or":[{"field":"bedroomNum","action":"match","value":"3"}]}]}},"uuid":"eb01ca40c178457430b9397c9913989b","type":"search","cost":18},"search_dig":{"timestamp":"1532591490000","fb_query_id":"74891355132325888","client":"app","uuid":"833FC61F-2F4C-48BA-B4D4-C6AECE3EDE20","time_local":"2018-07-26 15:52:24"}}
 
 }
