2bb2feff480ddae1900e61c6c04c8b4893ad102f
MagnetoWang <1527672216@qq.com>
2019-06-17 16:30:17 +0800

    修改内容：删除无用注释

--- paste/src/main/java/stringUtils/Constants.java
+++ paste/src/main/java/stringUtils/Constants.java
@@ -7,6 +7,6 @@ package stringUtils;
  * @create: 2018-07-30 21:00
  **/
 public class Constants {
-    public  String CLICKHOUSE_DRIVER="ru.yandex.clickhouse.ClickHouseDriver";
-    public  String CLICKHOUSE_ADDRESS1="jdbc:clickhouse://10.26.69.37:8123/search";
+    public  String CLICKHOUSE_DRIVER="xx.xxx.clickhouse.ClickHouseDriver";
+    public  String CLICKHOUSE_ADDRESS1="jdbc:clickhouse://xxx.xxx.xxx.xxx:xxxx/search";
 }
