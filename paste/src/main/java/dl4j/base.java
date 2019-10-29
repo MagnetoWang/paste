33c39c8fd7cd2985543c9c78f6d8d6d75278f6ae
MagnetoWang <1527672216@qq.com>
2019-10-29 10:48:47 +0800

    新增内容：dl4j的一系列包和基本使用

---
+++ paste/src/main/java/dl4j/base.java
@@ -0,0 +1,13 @@
+package dl4j;
+
+/**
+ * @Author wangzixian
+ * @Description TODO
+ * @Date 2019/10/28 17:44
+ **/
+public class base {
+    
+    public static void main(String[] args) {
+
+    }
+}
